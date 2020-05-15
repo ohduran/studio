89ec1d884dec75229aec684955aea0a1260c889d
ohduran <alvaro.duranb@hotmail.com>
2020-05-15 19:22:54 +0200

    onement I

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import StackedBarsIcon from "../components/StackedBarsIcon"
 import DefaultLayout from "../layouts"
 
 import thumbNailBookList from "../static/thumbnails/demos/BookList.png"
+import thumbNailOnementI from "../static/thumbnails/abex/onementI.png"
 import ListItem from "../components/ListItem"
 import ListSection from "../components/ListSection"
 
@@ -25,6 +26,15 @@ const IndexPage = () => {
           src={thumbNailBookList}
           title="Book List Demo"
           subtitle="Use of Context, Hooks and LocalStorage"
+        />
+      </ListSection>
+
+      <ListSection title="AbEx" className="" titleClassName="">
+        <ListItem
+          to="/onementI"
+          src={thumbNailOnementI}
+          title="Onement I"
+          subtitle="Barnett Newman"
         />
       </ListSection>
     </DefaultLayout>
---
+++ src/pages/onementI.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/onementI.css"
+
+const OnementI = () => {
+  return (
+    <div className="container">
+      <div id="vertical-line"></div>
+    </div>
+  )
+}
+
+export default OnementI
